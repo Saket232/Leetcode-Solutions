a5f5771c15adde1f2a8d302449dab56a38e00592
Saket232 <45016030+Saket232@users.noreply.github.com>
2024-11-09 21:53:12 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (35.37%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,53 +10,38 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) {
-            return list1;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        
+        ListNode s; // Pointer for the starting node of the merged list
+        if (list1.val <= list2.val) {
+            s = list1;
+            list1 = list1.next;
+        } else {
+            s = list2;
+            list2 = list2.next;
         }
         
-        if(list1 != null && list2 == null) {
-            return list1;
-        }
+        ListNode newHead = s; // Set the start of the merged list
         
-        if(list1 == null && list2 != null) {
-            return list2;
-        }
-        
-        ListNode s = new ListNode();
-        ListNode newHead = null; 
-        
-        if(list1!= null && list2 != null) {
-            if(list1.val <= list2.val) {
-                s = list1; 
-                list1 = s.next;
-            } else {
-                s = list2;
-                list2 = s.next;
-            }
-        }
-        
-        newHead = s; 
-        while(list1 != null && list2!= null) {
-            if(list1.val <= list2.val) {
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
                 s.next = list1;
-                s = list1;
-                list1 = s.next;
+                list1 = list1.next;
             } else {
                 s.next = list2;
-                s = list2;
-                list2 = s.next; 
+                list2 = list2.next;
             }
-            
+            s = s.next; // Move the pointer to the end of the merged list
         }
         
-        if(list1 == null) {
+        // At the end of the loop, attach any remaining nodes from either list1 or list2
+        if (list1 != null) {
+            s.next = list1;
+        } else {
             s.next = list2;
         }
         
-        if(list2 == null) {
-            s.next = list1;
-        }
-        
-        return newHead;
+        return newHead; // Return the head of the merged list
     }
 }
